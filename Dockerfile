50b72486d70273efa0965659ce0b6b1bfc4d140a
Drasko DRASKOVIC <drasko.draskovic@gmail.com>
2018-03-18 01:17:53 +0100

    Fix new dir
    Signed-off-by: Drasko DRASKOVIC <drasko.draskovic@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM golang:1.8-alpine AS builder
-WORKDIR /go/src/github.com/drasko/edgex-auth
+WORKDIR /go/src/monetasa
 COPY . .
 RUN cd cmd && CGO_ENABLED=0 GOOS=linux go build -ldflags "-s" -a -installsuffix cgo -o exe
 
 FROM scratch
-COPY --from=builder /go/src/github.com/mainflux/mainflux/cmd/http/exe /
-EXPOSE 8089
+COPY --from=builder /go/src/monetasa/cmd/monetasa/exe /
+EXPOSE 8080
 ENTRYPOINT ["/exe"]
--- service.go
+++ service.go
@@ -1,11 +1,11 @@
 package monetasa
 
-import "github.com/mainflux/mainflux/writer"
+import "monetasa/monetasa"
 
 // Service specifies an API that must be fullfiled by the domain service
 // implementation, and all of its decorators (e.g. logging & metrics).
 type Service interface {
 	// Publish accepts the raw SenML message and publishes it to the event bus
 	// for post processing.
-	Publish(writer.RawMessage) error
+	Publish(monetasa.RawMessage) error
 }
