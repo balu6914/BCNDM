e4c1980e41cc0972dc08a09248827f35390af388
Manuel Imperiale <manuel.imperiale@gmail.com>
2018-04-25 10:18:16 +0200

    Add TestLogin and TestIdentity
    Signed-off-by: Manuel Imperiale <manuel.imperiale@gmail.com>

--- auth/auth_test.go
+++ auth/auth_test.go
@@ -9,6 +9,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const wrong string = "wrong-value"
+
 var user auth.User = auth.User{"user@example.com", "password"}
 
 func newService() auth.Service {
@@ -34,5 +36,46 @@ func TestRegister(t *testing.T) {
 	for _, tc := range cases {
 		err := svc.Register(tc.user)
 		assert.Equal(t, tc.err, err, fmt.Sprintf("%s: expected %s got %s\n", tc.desc, tc.err, err))
+	}
+}
+
+func TestLogin(t *testing.T) {
+	svc := newService()
+	svc.Register(user)
+
+	cases := map[string]struct {
+		user auth.User
+		err  error
+	}{
+		"login with good credentials": {user, nil},
+		"login with wrong e-mail":     {auth.User{wrong, user.Password}, auth.ErrUnauthorizedAccess},
+		"login with wrong password":   {auth.User{user.Email, wrong}, auth.ErrUnauthorizedAccess},
+	}
+
+	for desc, tc := range cases {
+		_, err := svc.Login(tc.user)
+		assert.Equal(t, tc.err, err, fmt.Sprintf("%s: expected %s got %s\n", desc, tc.err, err))
+	}
+}
+
+func TestIdentity(t *testing.T) {
+	svc := newService()
+	svc.Register(user)
+	key, _ := svc.Login(user)
+
+	cases := map[string]struct {
+		key string
+		err error
+	}{
+		"valid token's identity":   {key, nil},
+		"invalid token's identity": {"", auth.ErrUnauthorizedAccess},
+	}
+
+	for desc, tc := range cases {
+		email, err := svc.Identity(tc.key)
+		if email != user.Email {
+			err = auth.ErrUnauthorizedAccess
+		}
+		assert.Equal(t, tc.err, err, fmt.Sprintf("%s: expected %s got %s\n", desc, tc.err, err))
 	}
 }
