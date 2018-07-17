537615cf1dbd0ecfee32229f995a8e037c479a3d
Manuel Imperiale <manuel.imperiale@gmail.com>
2018-07-17 17:22:19 +0200

    Typo fix
    Signed-off-by: Manuel Imperiale <manuel.imperiale@gmail.com>

--- ui/src/app/common/map/map.component.ts
+++ ui/src/app/common/map/map.component.ts
@@ -58,13 +58,13 @@ export class MapComponent implements OnInit {
 
   // Get stream type icon
   getIcon(type) {
-    var drinks = {
+    var icons = {
       temperature: "assets/img/icons/map-temp.svg",
       humidity: "assets/img/icons/map-water.svg",
       air: "assets/img/icons/map-co2.svg",
       default: "assets/img/icons/map.svg"
     };
-    return drinks[type] || drinks["default"];
+    return icons[type] || icons["default"];
   }
 
   // Display stream marker on a map
