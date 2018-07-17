6fa34aff04176f378aada70b05701e267fefde9e
Manuel Imperiale <manuel.imperiale@gmail.com>
2018-07-17 17:09:51 +0200

    Move and use map style from a separated json file
    Signed-off-by: Manuel Imperiale <manuel.imperiale@gmail.com>

--- ui/src/typings.d.ts
+++ ui/src/typings.d.ts
@@ -3,3 +3,9 @@ declare var module: NodeModule;
 interface NodeModule {
   id: string;
 }
+
+/* Map Wildcard Module */
+declare module "map-style.json" {
+    const value: any;
+    export default value;
+}
