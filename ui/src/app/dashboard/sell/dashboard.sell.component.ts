e24a2500bd388a612e2b186e7b2bdc4fbce22029
dusanb <dusan.borovcanin@mainflux.com>
2018-08-01 15:12:47 +0000

    Update streams service

--- ui/src/app/dashboard/sell/dashboard.sell.component.ts
+++ ui/src/app/dashboard/sell/dashboard.sell.component.ts
@@ -6,57 +6,53 @@ import { DashboardSellAddComponent } from './add/dashboard.sell.add.component';
 import { StreamService } from '../../common/services/stream.service';
 import { AuthService } from '../../auth/services/auth.service';
 import { Table, TableType } from '../../shared/table/table';
+import { Query } from '../../common/interfaces/query.interface';
+import { Page } from '../../common/interfaces/page.interface';
+import { Stream } from '../../common/interfaces';
 
 @Component({
   selector: 'dashboard-sell',
   templateUrl: './dashboard.sell.component.html',
-  styleUrls: [ './dashboard.sell.component.scss' ]
+  styleUrls: ['./dashboard.sell.component.scss']
 })
 export class DashboardSellComponent {
-    user: any;
-    temp = [];
-    streams = [];
-    table: Table = new Table();
+  user: any;
+  temp = [];
+  streams = [];
+  table: Table = new Table();
 
-    constructor(
-        private streamService: StreamService,
-        private AuthService: AuthService,
-        private router: Router,
-        private modalService: BsModalService,
-    ) {
-    }
+  constructor(
+    private streamService: StreamService,
+    private AuthService: AuthService,
+    private router: Router,
+    private modalService: BsModalService,
+  ) {
+  }
 
   ngOnInit() {
     // Fetch current User
     this.user = {};
     this.AuthService.getCurrentUser().subscribe(
-        data =>  {
-            this.user = data;
-        },
-        err => {
-            console.log(err)
-        }
+      data => {
+        this.user = data;
+      },
+      err => {
+        console.log(err)
+      }
     );
 
     // Config table
-    this.table.title = "Streams";
+    this.table.title = 'Streams';
     this.table.tableType = TableType.Sell;
-    this.table.headers = ["Stream Name", "Stream Type","Stream Price"];
+    this.table.headers = ['Stream Name', 'Stream Type', 'Stream Price'];
 
-    // TODO: Replace this coordinates with map bounds
-    const southWestLng = -180;
-    const southWestLat = -90;
-    const northEastLng = 180;
-    const northEastLat = 90;
+    const query = new Query();
 
-    // Search streams on drawed region
-    this.streamService.searchStreams(
-      "geo", southWestLng, southWestLat, southWestLng, northEastLat,
-      northEastLng, northEastLat, northEastLng, southWestLat).subscribe(
-      (result: any) => {
-        this.temp = [...result.Streams];
-        result.Streams.forEach(stream => {
-          if (stream.owner == this.user.id) {
+    this.streamService.searchStreams(query).subscribe(
+      (result: Page<Stream>) => {
+        this.temp = result.content;
+        result.content.forEach(stream => {
+          if (stream.owner === this.user.id) {
             this.streams.push(stream);
           }
         });
@@ -81,7 +77,7 @@ export class DashboardSellComponent {
       this.streamService.addStreamBulk(formData).subscribe(
         data => this.router.navigate(['/dashboard/sell']),
         error => console.log(error),
-    );
+      );
     }
   }
 
