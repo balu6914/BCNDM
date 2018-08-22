9662fdecf0437daf2dbacd68ea5415e4ab52b668
nmarcetic <n.marcetic86@gmail.com>
2018-08-22 19:57:50 +0200

    Added action buttons edti/delete/buy on stream details

--- ui/src/app/shared/table/details/table.row.details.component.ts
+++ ui/src/app/shared/table/details/table.row.details.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 
 import { Stream } from '../../../common/interfaces/stream.interface';
+import { TasPipe } from '../../../common/pipes/converter.pipe';
+import { TableType } from '../table';
+import { DashboardSellEditComponent } from '../../../dashboard/sell/edit';
+import { DashboardSellDeleteComponent } from '../../../dashboard/sell/delete';
+import { DashboardBuyAddComponent } from '../../../dashboard/buy/add';
 
 @Component({
   selector: 'dpc-table-row-details',
@@ -9,11 +16,19 @@ import { Stream } from '../../../common/interfaces/stream.interface';
 })
 
 export class TableRowDetailsComponent implements OnInit {
+  types = TableType;
+  bsModalRef: BsModalRef;
 
-  @Input() stream: Stream;
+  @Input() stream: any;
+  @Input() rowType: TableType;
   @Output() backClicked = new EventEmitter<String>();
+  @Output() deleteEvt: EventEmitter<any> = new EventEmitter();
+  @Output() editEvt: EventEmitter<any> = new EventEmitter();
 
   constructor(
+    private modalService: BsModalService,
+    private tasPipe: TasPipe,
+
   ) {}
 
   ngOnInit() {
@@ -23,5 +38,62 @@ export class TableRowDetailsComponent implements OnInit {
     this.backClicked.emit('trigger');
   }
 
+  openModalEdit() {
+    // Parameters editData and streamID are used in DashboardSellEditComponent
+    const initialState = {
+      editData: {
+        name:        this.stream.name,
+        type:        this.stream.type,
+        description: this.stream.description,
+        url:         "fakeurl.com", //TODO: use stream URL
+        price:       this.tasPipe.transform(this.stream.price),
+        long:        this.stream.location.coordinates[0],
+        lat:         this.stream.location.coordinates[1],
+      },
+      streamID: this.stream.id,
+    };
+    // Open DashboardSellEditComponent as Modal
+    this.bsModalRef = this.modalService.show(DashboardSellEditComponent, {initialState})
+      .content.streamEdited.subscribe(
+        stream => {
+          this.editEvt.emit(stream);
+        }
+      );
+  }
+
+  openModalDelete() {
+    // Parameter stream is used in DashboardSellDeleteComponent
+    const initialState = {
+      stream: {
+        id:          this.stream.id,
+        name:        this.stream.name,
+        type:        this.stream.type,
+        description: this.stream.description,
+        price:       this.tasPipe.transform(this.stream.price),
+      },
+    };
+
+    // Open DashboardSellDeleteComponent as Modal
+    this.bsModalRef = this.modalService.show(DashboardSellDeleteComponent, {initialState})
+      .content.streamDeleted.subscribe(
+        id => {
+          // Emit event to TableComponent
+          this.deleteEvt.emit(id);
+        }
+      );
+    }
+
+  openModalSubscription() {
+    // Parameter stream is set on modal component
+    const initialState = {
+      stream: {
+        id:         this.stream.id,
+        name:       this.stream.name,
+        price:      this.stream.price,
+      },
+    };
+    // Open DashboardSellAddComponent Modal
+    this.bsModalRef = this.modalService.show(DashboardBuyAddComponent, {initialState});
+  }
 
 }
