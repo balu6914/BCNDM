652f374e9d730f906c3e831464fd37ae84a2a246
nmarcetic <n.marcetic86@gmail.com>
2018-09-03 16:36:20 +0200

    Fixed Observable import

--- ui/src/app/common/services/subscription.service.ts
+++ ui/src/app/common/services/subscription.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
