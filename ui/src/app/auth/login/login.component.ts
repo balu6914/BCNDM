a26e48c28115f8f2033d7668781f0058b08e11a6
nmarcetic <n.marcetic86@gmail.com>
2018-09-07 16:18:49 +0200

    Fixed http 403 interceptor and login error message

--- ui/src/app/auth/login/login.component.ts
+++ ui/src/app/auth/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl, ReactiveFormsModule, FormBuilder, Validators  } from '@angular/forms';
 
 import {Router} from '@angular/router';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 import { User } from '../../common/interfaces/user.interface';
 import { UserService } from '../services/user.service';
@@ -10,7 +12,7 @@ import { AuthService } from '../services/auth.service';
 const emailValidator = Validators.pattern('^[a-z]+[a-z0-9._]+@[a-z]+\.[a-z.]{2,5}$');
 
 @Component({
-  selector: 'login-form',
+  selector: 'dpc-login-form',
   templateUrl: './login.component.html',
   styleUrls: [ './login.component.scss' ],
   providers: [
@@ -41,7 +43,7 @@ export class LoginComponent {
 
     onSubmit(model: User, isValid: boolean) {
         this.errorMsg = null;
-        if(isValid) {
+        if (isValid) {
             this.AuthService.login(model.email, model.password)
               .subscribe(
                 (token: any) => {
