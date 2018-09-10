b42be1ca07c214dbad679f02536a498631bf6356
Dušan Borovčanin <borovcanindusan1@gmail.com>
2018-09-10 11:41:07 +0200

    Fix too small page size
    Change page size from 20 to 50.
    
    Signed-off-by: Dušan Borovčanin <borovcanindusan1@gmail.com>

--- ui/src/app/dashboard/main/dashborad.main.component.ts
+++ ui/src/app/dashboard/main/dashborad.main.component.ts
@@ -22,7 +22,7 @@ export class DashboardMainComponent implements OnInit {
   streamTypes: StreamsType;
   activeStreamsSection: StreamSection = new StreamSection();
   page = 0;
-  limit = 20;
+  limit = 50;
 
   constructor(
     private AuthService: AuthService,
