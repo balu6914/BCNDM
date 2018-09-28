17efb56435335555e956a165698cd19aca9faf52
Manuel Imperiale <manuel.imperiale@gmail.com>
2018-09-28 12:10:18 +0200

    Fix clear-filter and coding style
    Signed-off-by: Manuel Imperiale <manuel.imperiale@gmail.com>

--- ui/src/app/shared/filters/sidebar-filters/sidebar.filters.component.ts
+++ ui/src/app/shared/filters/sidebar-filters/sidebar.filters.component.ts
@@ -1,6 +1,6 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
-import { Query } from '../../../common/interfaces/query.interface';
+import { Query } from 'app/common/interfaces/query.interface';
 
 @Component({
   selector: 'dpc-sidebar-filters',
@@ -8,31 +8,28 @@ import { Query } from '../../../common/interfaces/query.interface';
   styleUrls: [ './sidebar.filters.component.scss' ]
 })
 export class SidebarFiltersComponent {
-    _opened: boolean = false
+    _opened = false;
     form: FormGroup;
-    // TODO: Remove mocked cities
-    mockCities: string[];
+
     @Output()
     // Emit event when we successfully buy more token , to get updated balance.
     filtersUpdate = new EventEmitter();
-    public selectCityControl = new FormControl()
+    public selectCityControl = new FormControl();
 
     constructor(
       private formBuilder: FormBuilder,
     ) {
-
-       this.mockCities = ['Belgrade', 'Paris', 'Tel Aviv'];
        // Update form control city value on selected value change
        this.selectCityControl.valueChanges.subscribe(value =>
          this.form.patchValue({city: value})
        );
 
        this.form = formBuilder.group({
-         name: ['', String],
-         streamType: ['', String],
+         name: [''],
+         streamType: [''],
          minPrice: [0, Number],
          maxPrice: [1000000, Number]
-       })
+       });
     }
 
     _toggleSidebar() {
@@ -41,11 +38,12 @@ export class SidebarFiltersComponent {
 
     onClear() {
       this.form.reset();
+      this.onSubmit();
     }
 
-    onSubmit(isValid: boolean) {
-    if(isValid) {
-        this.filtersUpdate.emit(this.form.value)
+    onSubmit() {
+      if (this.form.valid) {
+        this.filtersUpdate.emit(this.form.value);
       }
     }
 
