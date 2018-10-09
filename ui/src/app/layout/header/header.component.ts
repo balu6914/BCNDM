3050ee129395bca201064f33ddfd1359abbcc731
nmarcetic <n.marcetic86@gmail.com>
2018-10-09 16:48:53 +0200

    Fixed Auth service

--- ui/src/app/layout/header/header.component.ts
+++ ui/src/app/layout/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ISubscription } from 'rxjs/Subscription';
+
 import { AuthService } from '../../auth/services/auth.service';
 import { BalanceService } from '../../shared/balance/balance.service';
 import { Balance } from '../../common/interfaces/balance.interface';
@@ -11,17 +13,20 @@ import { Balance } from '../../common/interfaces/balance.interface';
   ],
 })
 
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   isLoggedin: Boolean;
   user: any;
   balance = new Balance();
-
+  private userSubscription: ISubscription;
+  private loggedInSubscription: ISubscription;
+  private balanceSubscription: ISubscription;
+  private balanceChangeSubscription: ISubscription;
   constructor(
     private AuthService: AuthService,
     private balanceService: BalanceService,
   ) {
     // Listen to balance changed events
-    this.balanceService.balance.subscribe(result => {
+    this.balanceChangeSubscription = this.balanceService.balance.subscribe(result => {
       this.balance = result;
     },
   );
@@ -29,24 +34,29 @@ export class HeaderComponent implements OnInit {
    }
 
   ngOnInit() {
-    this.AuthService.loggedIn
+    this.loggedInSubscription = this.AuthService.loggedIn
       .subscribe(is => {
         this.isLoggedin = is;
-
-        this.AuthService.getCurrentUser()
+        if (this.isLoggedin) {
+          this.userSubscription = this.AuthService.getCurrentUser()
           .subscribe(data => {
-            console.log("I have data", data)
-              this.user = data;
-              // Get user balance
-              if (data && data.id) {
-                this.getBalance();
-              }
+            this.user = data;
+            // Get user balance
+            this.getBalance();
           });
+        }
       });
     }
 
+    ngOnDestroy() {
+      this.userSubscription.unsubscribe();
+      this.loggedInSubscription.unsubscribe();
+      this.balanceSubscription.unsubscribe();
+      this.balanceChangeSubscription.unsubscribe();
+    }
+
   getBalance() {
-      this.balanceService.get().subscribe(
+      this.balanceSubscription = this.balanceService.get().subscribe(
         (result: any) => {
           this.balance.amount = result.balance;
           //TODO remove this Mock of fiatAmount when we add this info on API side
@@ -55,7 +65,7 @@ export class HeaderComponent implements OnInit {
           this.balanceService.changed(this.balance);
         },
         err => {
-          console.error("Error fetching user balance ", err)
+          console.error('Error fetching user balance ', err);
         });
   }
 
