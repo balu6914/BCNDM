f88bc73cdf6e6fe91169f87cdb2c637c519392a1
Khalil <mina.khalil@nokia.com>
2019-03-06 12:12:58 +0100

    DP-192 - Add User first_name and last_name Validators to signup form
    Resolves #192

--- ui/src/app/auth/signup/signup.component.ts
+++ ui/src/app/auth/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, Output, EventEmitter } from '@angular/core';
-import { FormGroup, FormControl, ReactiveFormsModule, FormBuilder, Validators  } from '@angular/forms';
+import { FormGroup, FormControl, ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
@@ -11,7 +11,7 @@ import { AuthService } from 'app/auth/services/auth.service';
 @Component({
   selector: 'dpc-signup-form',
   templateUrl: './signup.component.html',
-  styleUrls: [ './signup.component.scss' ],
+  styleUrls: ['./signup.component.scss'],
   providers: [
   ],
 })
@@ -26,35 +26,35 @@ export class SignupComponent {
     private router: Router,
     private UserService: UserService,
     private AuthService: AuthService
-    ) {
-    }
+  ) {
+  }
 
-    ngOnInit() {
-      this.errorMsg = null;
-      this.form = this.fb.group({
-        email:    ['', [Validators.required, Validators.email, Validators.maxLength(32)]],
-        password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(16)]],
-        confirm:  [''],
-        first_name:     ['', [Validators.maxLength(16)]],
-        last_name:  ['', [Validators.maxLength(16)]]
-      },
+  ngOnInit() {
+    this.errorMsg = null;
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email, Validators.maxLength(32)]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(16)]],
+      confirm: [''],
+      first_name: ['', [Validators.required, Validators.maxLength(32)]],
+      last_name: ['', [Validators.required, Validators.maxLength(32)]]
+    },
       {
         validator: this.passwordMatchValidator
       });
-    }
+  }
 
-    passwordMatchValidator(fg: FormGroup) {
-      // Compare passwords only if minLength is valid
-      if (fg.get('confirm').value.length > 0) {
+  passwordMatchValidator(fg: FormGroup) {
+    // Compare passwords only if minLength is valid
+    if (fg.get('confirm').value.length > 0) {
       if (fg.get('password').value === fg.get('confirm').value) {
-          return null;
-        }
-        fg.controls.confirm.setErrors({'invalid': true});
+        return null;
       }
-
-      // Set form confirm password missmatch
-      return {'missmatch': true };
+      fg.controls.confirm.setErrors({ 'invalid': true });
     }
+
+    // Set form confirm password missmatch
+    return { 'missmatch': true };
+  }
 
   onSubmit() {
     this.submitted = true;
@@ -69,10 +69,10 @@ export class SignupComponent {
       this.UserService.addUser(user).subscribe(
         response => {
           this.AuthService.login(user.email, user.password)
-          .subscribe(
-            token => this.router.navigate(['/dashboard']),
-            err => this.errorMsg = err.status
-          );
+            .subscribe(
+              token => this.router.navigate(['/dashboard']),
+              err => this.errorMsg = err.status
+            );
         },
         err => {
           // Handle tmp case when user already exists and we don't have error msg on API side yet.
@@ -81,7 +81,7 @@ export class SignupComponent {
           } else {
             this.errorMsg = err.status;
           }
-      });
+        });
     }
   }
 }
