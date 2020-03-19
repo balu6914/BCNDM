7405ccaea66e7a5345af43c99449a5f061745641
Sava Radoš <sava@zorbiro.com>
2020-03-19 13:08:57 +0100

    Remove old proxy (#297)
    Signed-off-by: Sava Radoš <sava@zorbiro.com>

--- proxy/lib/config.js
+++
@@ -1,8 +0,0 @@
-const appConfig = {
-    secret: 'ynehj45le70cfbrcsszm', // JWT secret key
-    issuerName: 'Datapace',
-    proxyURL: process.env.PUBLIC_URL || 'http://localhost:8081', // Public access UR
-    redisUrl: process.env.REDIS_URL || 'redis://localhost:6379',
-};
-
-module.exports = appConfig;
