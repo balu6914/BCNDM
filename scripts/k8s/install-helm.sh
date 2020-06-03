d222f43a19506efaeeeb6cdb116657202f9d1d57
Timo Ahokas <timo.ahokas@lekane.com>
2020-06-03 16:50:28 +0300

    add support for configuring user & home directory

--- scripts/k8s/install-helm.sh
+++ scripts/k8s/install-helm.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 . env.sh
-scp /tmp/helm.tar.gz ubuntu\@$DATAPACEHOSTIP:/home/ubuntu/
+scp /tmp/helm.tar.gz $DATAPACEHOSTUSER\@$DATAPACEHOSTIP:$DATAPACEHOSTHOME/
 if [ $? -ne 0 ]; then
     echo "failed to copy helm.tar.gz from /tmp, aborting"
     exit 1
 fi
 echo "installing datapace helm chart"
-ssh ubuntu@$DATAPACEHOSTIP << EOF
-  /snap/bin/microk8s.config > /home/ubuntu/kubeconfig
+ssh $DATAPACEHOSTUSER@$DATAPACEHOSTIP << EOF
+  /snap/bin/microk8s.config > $DATAPACEHOSTHOME/kubeconfig
   /snap/bin/helm repo add bitnami https://charts.bitnami.com/bitnami
   tar -xf helm.tar.gz
   cd helm/datapace
@@ -15,6 +15,6 @@ ssh ubuntu@$DATAPACEHOSTIP << EOF
   sed -i 's/dproxy.datapace.local$/$DPROXYHOSTNAME/g' values.yaml
   sed -i 's/ip: 10.0.0.1$/ip: $BCHOSTIP/g' values.yaml
   /snap/bin/helm dependency build
-  /snap/bin/helm install dpd . --set imageCredentials.username=$GITLABUSER --set imageCredentials.password=$GITLABTOKEN --kubeconfig /home/ubuntu/kubeconfig
+  /snap/bin/helm install dpd . --set imageCredentials.username=$GITLABUSER --set imageCredentials.password=$GITLABTOKEN --kubeconfig $DATAPACEHOSTHOME/kubeconfig
 EOF
 echo "datapace helm chart done"
