9d95257fc849e006aa68d616f9fa8ae8ce7769b0
Sava Radoš <sava@zorbiro.com>
2020-07-21 21:52:44 +0200

    Fix/nocr/fabric run script (#361)
    * fix fabric run script so it works as described in the document
    
    * remove optional docker compose for fabric explorer
    
    Co-authored-by: Drasko DRASKOVIC <drasko.draskovic@gmail.com>

--- docker/fabric/run.sh
+++ docker/fabric/run.sh
@@ -3,7 +3,7 @@
 # This script will run docker composition and clean old ghost containers
 
 # Stop all containers (if running)
-docker-compose -f docker/fabric/docker-compose.yaml stop
+docker-compose -f docker/fabric/docker-compose.yml stop
 
 # Remove old keystore (bug with fabric, container must be removed)
 docker rm $(docker ps -a | grep -v "fabric-ca-pgsql" | awk '{print $1}')
