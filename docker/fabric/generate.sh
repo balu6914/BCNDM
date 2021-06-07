5c61d11c1999d3e2a7b8ab81c02f8ad14c6b90da
Dušan Borovčanin <dusan.borovcanin@mainflux.com>
2021-06-07 22:54:21 +0200

    Fix generate script (#508)
    Co-authored-by: dusanb <borovcanindusan1@gmail.com.com>

--- docker/fabric/generate.sh
+++ docker/fabric/generate.sh
@@ -44,14 +44,4 @@ echo "### Generating network artifacts"
 FABRIC_CFG_PATH=$FABRIC_CFG_PATH configtxgen -outputBlock $GENESIS_BLOCK_PATH -profile $GENESIS_BLOCK_PROFILE -channelID $CHSYS_ID
 FABRIC_CFG_PATH=$FABRIC_CFG_PATH configtxgen -outputCreateChannelTx $CH_OUTPUT_PATH -profile $CH_PROFILE -channelID $CH_ID
 
-###
-# Update docker-compose
-###
-NEW_CERT_NAME=`find $CRYPTO_CONF_DIR/peerOrganizations/org1.datapace.com/ca/ -type f -name "*_sk" | xargs basename`
-sed -i -- "s#\(FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config\)/.*#\1/$NEW_CERT_NAME#" $BASE_COMPOSE_FILE
-sed -i -- "s#\(FABRIC_CA_SERVER_TLS_KEYFILE=/etc/hyperledger/fabric-ca-server-config\)/.*#\1/$NEW_CERT_NAME#" $BASE_COMPOSE_FILE
-NEW_CERT_ADMIN=`find $CRYPTO_CONF_DIR/peerOrganizations/org1.datapace.com/users/Admin@org1.datapace.com/msp/keystore/ -type f -name "*_sk" | xargs basename`
-sed -i -- "s#\(/tmp/crypto/peerOrganizations/org1.datapace.com/users/Admin@org1.datapace.com/msp/keystore\)/.*#\1/$NEW_CERT_ADMIN\"#" $EXPLORER_CONNECTION_FILE
-
-
 echo "Success! All done."
