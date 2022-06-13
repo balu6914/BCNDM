a879b83b12e6a35e3a0d6a9c90192ef0c7ca71de
balu6914 <balaji.mvshyd@gmail.com>
2022-06-13 05:43:32 +0300

    EECLOUD-8162

--- cmd/dproxy/main.go
+++ cmd/dproxy/main.go
@@ -80,7 +80,7 @@ type config struct {
 }
 
 func main() {
-	cfg := loadConfig(log.Logger)
+	cfg := loadConfig()
 	logger := logger.New(os.Stdout)
 	errs := make(chan error, 2)
 	eventsRepository, err := connectToEventsRepository()
